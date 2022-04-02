7ae74b215b05b6957393e357bb08fdeb49facde2
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-02 23:27:27 +0300

    full

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 const errorHandler = require('./middlewares/errorHadler');
 const router = require('./routes/index');
-const cors = require('./middlewares/cors');
+//const cors = require('./middlewares/cors');
+const cors = require('cors');
 
 const DB_ADDRES = require('./utils/config');
 const { PORT = 3000, DB_URL, NODE_ENV } = process.env;
@@ -19,13 +20,43 @@ mongoose.connect(NODE_ENV === 'production' ? DB_URL : DB_ADDRES, {
   useFindAndModify: false,
 });
 
-console.log(cors);
+const allowedCors = [
+  'http://localhost:3000',
+  'http://diplom.ilkras.nomoredomains.work/'
+];
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(requestLogger);
-app.use(cors);
+
 app.use(router);
 
 app.use('*', () => {
