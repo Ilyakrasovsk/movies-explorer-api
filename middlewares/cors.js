11a94a5f2c64b1a78267c769095d061f9e586026
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-02 19:39:27 +0300

    full

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,8 +1,9 @@
 const allowedCors = [
   'http://localhost:3000',
-  'https://localhost:3000',
-  'https://diplom.ilkras.nomoredomains.work/',
   'http://diplom.ilkras.nomoredomains.work/',
+  'https://diplom.ilkras.nomoredomains.work/',
+  'http://api.diplom.ilkras.nomoredomains.work/',
+  'https://api.diplom.ilkras.nomoredomains.work/',
 ];
 
 const cors = (req, res, next) => {
