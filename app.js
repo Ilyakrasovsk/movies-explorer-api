1e4eca4ec82f99d0eb52a39e8eb6aa2ef58bc539
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-02 16:23:24 +0300

    full

--- app.js
+++ app.js
@@ -7,41 +7,24 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/not-found-error');
 const errorHandler = require('./middlewares/errorHadler');
 const router = require('./routes/index');
-const DB_ADDRES = require('./utils/config');
-
 const cors = require('./middlewares/cors');
 
-const app = express();
+const DB_ADDRES = require('./utils/config');
 const { PORT = 3000, DB_URL, NODE_ENV } = process.env;
 
-const allowedCors = [
-  'http://localhost:3000',
-  'https://api.diplom.ilkras.nomoredomains.work/',
-  'http://api.diplom.ilkras.nomoredomains.work/',
-  'https://diplom.ilkras.nomoredomains.work/',
-  'http://diplom.ilkras.nomoredomains.work/',
-];
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  next();
+const app = express();
+mongoose.connect(NODE_ENV === 'production' ? DB_URL : DB_ADDRES, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+console.log(cors);
+
+app.use(cors);
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-//app.use(cors);
 
 app.use(requestLogger);
 
@@ -55,11 +38,6 @@ app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
-mongoose.connect(NODE_ENV === 'production' ? DB_URL : DB_ADDRES, () => {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
 
 app.listen(PORT, () => {
   console.log(`Ссылка на сервер: ${PORT}`);
