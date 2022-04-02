6ff9c854fde856b0cbc43f92e009e7e9a526eab8
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-02 19:03:04 +0300

    full

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -19,7 +19,8 @@ const cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    return
+    res.end();
   }
 
   return next();
