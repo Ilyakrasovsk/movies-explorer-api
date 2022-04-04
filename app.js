1e6aad9fa4a0ef38accd994c37ba6bf777b30f13
Ilya Krasovsky <ilyacrasovsky@yandex.ru>
2022-04-04 03:24:00 +0300

    устал

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const NotFoundError = require('./errors/not-found-error');
 const errorHandler = require('./middlewares/errorHadler');
 const router = require('./routes/index');
 //const cors = require('./middlewares/cors');
-const cors = require('cors');
+//const cors = require('cors');
 
 const DB_ADDRES = require('./utils/config');
 const { PORT = 3000, DB_URL, NODE_ENV } = process.env;
@@ -22,9 +22,10 @@ mongoose.connect(NODE_ENV === 'production' ? DB_URL : DB_ADDRES, {
 
 const allowedCors = [
   'http://localhost:3000',
-  'http://api.diplom.ilkras.nomoredomains.work/',
-  'https://api.diplom.ilkras.nomoredomains.work/',
+  'http://diplom.ilkras.nomoredomains.work',
+  'https://diplom.ilkras.nomoredomains.work',
 ];
+// eslint-disable-next-line consistent-return
 app.use((req, res, next) => {
   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
   // проверяем, что источник запроса есть среди разрешённых
@@ -32,7 +33,14 @@ app.use((req, res, next) => {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', '*');
   }
-  const { method } = req;
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  }
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы с этими заголовками
